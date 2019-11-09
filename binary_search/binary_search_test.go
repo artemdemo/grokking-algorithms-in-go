3f77653c86764c8f960a8a3fbe3f249b9febcd28
Artem Demo <artemdemo@gmail.com>
2019-11-09 17:32:40 +0200

    added testSuits()

--- binary_search/binary_search_test.go
+++ binary_search/binary_search_test.go
@@ -2,41 +2,48 @@ package binary_search
 
 import "testing"
 
-func Test_BinarySearch(t *testing.T) {
+type binarySearchType = func(list []int, item int) (int, bool)
+
+func testSuits(t *testing.T, binarySearch binarySearchType) {
     t.Run("itemNotIncluded", func(t *testing.T) {
         list := []int{1,2,3}
-        result, ok := BinarySearch(list, 4)
+        result, ok := binarySearch(list, 4)
 
         if ok == true {
-            t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
+            t.Fatalf("binarySearch() should return `ok=false`, got \"%v\" instead", ok)
         }
         if result != 0 {
-            t.Fatalf("BinarySearch() should return `result=0`, got \"%v\" instead", result)
+            t.Fatalf("binarySearch() should return `result=0`, got \"%v\" instead", result)
         }
     })
 
     t.Run("itemIncludedOnTheRight", func(t *testing.T) {
         list := []int{1,2,3,4,5,6,7,9}
-        result, ok := BinarySearch(list, 7)
+        result, ok := binarySearch(list, 7)
 
         if ok == false {
-            t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
+            t.Fatalf("binarySearch() should return `ok=false`, got \"%v\" instead", ok)
         }
         if result != 6 {
-            t.Fatalf("BinarySearch() should return `result=6`, got \"%v\" instead", result)
+            t.Fatalf("binarySearch() should return `result=6`, got \"%v\" instead", result)
         }
     })
 
     t.Run("itemIncludedOnTheLeft", func(t *testing.T) {
         t.Name()
         list := []int{1,2,3,4,5,6,7,9}
-        result, ok := BinarySearch(list, 2)
+        result, ok := binarySearch(list, 2)
 
         if ok == false {
-            t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
+            t.Fatalf("binarySearch() should return `ok=false`, got \"%v\" instead", ok)
         }
         if result != 1 {
-            t.Fatalf("BinarySearch() should return `result=1`, got \"%v\" instead", result)
+            t.Fatalf("binarySearch() should return `result=1`, got \"%v\" instead", result)
         }
     })
 }
+
+func Test_BinarySearch(t *testing.T) {
+    testSuits(t, BinarySearch)
+}
+
