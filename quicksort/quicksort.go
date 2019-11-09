e821c3989bce0762f296378960c6a7c51669e7c7
Artem Demo <artemdemo@gmail.com>
2019-11-09 19:03:45 +0200

    updagraded quicksort - added randomly selected pivot

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -1,13 +1,21 @@
 package quicksort
 
+import (
+    "math/rand"
+    "time"
+)
+
 func Quicksort(arr []int) []int {
     if len(arr) < 2 {
         return arr
     }
-    pivot := arr[0]
+    rand.Seed(time.Now().Unix())            // initialize global pseudo random generator
+    pivotIndex := rand.Intn(len(arr) - 1)   // selecting random element from a given array
+    pivot := arr[pivotIndex]
     var less []int
     var greater []int
-    for _, num := range arr[1:] {
+    arrWithoutPivot := append(arr[:pivotIndex], arr[pivotIndex+1:]...)
+    for _, num := range arrWithoutPivot {
         if num <= pivot {
             less = append(less, num)
         } else {
