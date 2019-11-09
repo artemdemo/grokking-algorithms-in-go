5d1e658f413282133dee70869b0db0a273244828
Artem Demo <artemdemo@gmail.com>
2019-11-09 17:09:26 +0200

    updated test

--- binary_search/binary_search_test.go
+++ binary_search/binary_search_test.go
@@ -2,44 +2,41 @@ package binary_search
 
 import "testing"
 
-func itemNotIncluded(t *testing.T) {
-    list := []int{1,2,3}
-    result, ok := BinarySearch(list, 4)
-
-    if ok == true {
-        t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
-    }
-    if result != 0 {
-        t.Fatalf("BinarySearch() should return `result=0`, got \"%v\" instead", result)
-    }
-}
-
-func itemIncludedOnTheRight(t *testing.T) {
-    list := []int{1,2,3,4,5,6,7,9}
-    result, ok := BinarySearch(list, 7)
-
-    if ok == false {
-        t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
-    }
-    if result != 6 {
-        t.Fatalf("BinarySearch() should return `result=6`, got \"%v\" instead", result)
-    }
-}
-
-func itemIncludedOnTheLeft(t *testing.T) {
-    list := []int{1,2,3,4,5,6,7,9}
-    result, ok := BinarySearch(list, 2)
-
-    if ok == false {
-        t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
-    }
-    if result != 1 {
-        t.Fatalf("BinarySearch() should return `result=1`, got \"%v\" instead", result)
-    }
-}
-
 func Test_BinarySearch(t *testing.T) {
-    itemNotIncluded(t)
-    itemIncludedOnTheRight(t)
-    itemIncludedOnTheLeft(t)
+    t.Run("itemNotIncluded", func(t *testing.T) {
+        list := []int{1,2,3}
+        result, ok := BinarySearch(list, 4)
+
+        if ok == true {
+            t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
+        }
+        if result != 0 {
+            t.Fatalf("BinarySearch() should return `result=0`, got \"%v\" instead", result)
+        }
+    })
+
+    t.Run("itemIncludedOnTheRight", func(t *testing.T) {
+        list := []int{1,2,3,4,5,6,7,9}
+        result, ok := BinarySearch(list, 7)
+
+        if ok == false {
+            t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
+        }
+        if result != 6 {
+            t.Fatalf("BinarySearch() should return `result=6`, got \"%v\" instead", result)
+        }
+    })
+
+    t.Run("itemIncludedOnTheLeft", func(t *testing.T) {
+        t.Name()
+        list := []int{1,2,3,4,5,6,7,9}
+        result, ok := BinarySearch(list, 2)
+
+        if ok == false {
+            t.Fatalf("BinarySearch() should return `ok=false`, got \"%v\" instead", ok)
+        }
+        if result != 1 {
+            t.Fatalf("BinarySearch() should return `result=1`, got \"%v\" instead", result)
+        }
+    })
 }
