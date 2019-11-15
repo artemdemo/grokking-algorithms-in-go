83704f20e94cbcd3ddeb7d436e5d7c48d7ac6ff1
Artem Demo <artemdemo@gmail.com>
2019-11-15 19:15:34 +0200

    run tests with coverage html output

--- tests.sh
+++ tests.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-echo "binary_search/*"
-go test ./binary_search -test.v -cover
+#echo "binary_search/*"
+#go test ./binary_search -test.v -cover
+#
+#echo "quicksort/*"
+#go test ./quicksort -test.v -cover
+#
+#echo "tree/*"
+#go test ./tree -test.v -cover
 
-echo "quicksort/*"
-go test ./quicksort -test.v -cover
-
-echo "tree/*"
-go test ./tree -test.v -cover
+# In order to run test with coverage,
+# you need to use following commands:
+go test ./tree ./binary_search ./quicksort -test.v -cover -coverprofile="coverage.out"
+go tool cover -html="coverage.out"
