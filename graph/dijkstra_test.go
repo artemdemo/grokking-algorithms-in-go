7f3a4f34746a8e3f3e64addfa038e77aaf745e30
Artem Demo <artemdemo@gmail.com>
2019-11-22 13:11:07 +0200

    added calcBookExample test

--- graph/dijkstra_test.go
+++ graph/dijkstra_test.go
@@ -44,4 +44,35 @@ func Test_Dijkstra(t *testing.T) {
             t.Fatalf("Dijkstra() should return path=`%v`, got \"%v\" instead", expectedResult, resultStr)
         }
     })
+
+    t.Run("calcBookExample", func(t *testing.T) {
+        nodeStart := NewNode("Start")
+        nodeA := NewNode("A")
+        nodeB := NewNode("B")
+        nodeFin := NewNode("Fin")
+
+        nodeStart.addConnection(nodeA, 6)
+        nodeStart.addConnection(nodeB, 2)
+        nodeB.addConnection(nodeA, 3)
+        nodeB.addConnection(nodeFin, 5)
+        nodeA.addConnection(nodeFin, 1)
+
+        path, price, ok := Dijkstra(nodeStart, nodeFin)
+
+        expectedOk := true
+        if ok != expectedOk {
+            t.Fatalf("Dijkstra() should return ok=`%v`, got \"%v\" instead", expectedOk, ok)
+        }
+
+        expectedPrice := 6
+        if price != float64(expectedPrice) {
+            t.Fatalf("Dijkstra() should return price=`%v`, got \"%v\" instead", expectedPrice, price)
+        }
+
+        resultStr := pathToString(path)
+        expectedResult := "[Start B A Fin]"
+        if resultStr != expectedResult {
+            t.Fatalf("Dijkstra() should return path=`%v`, got \"%v\" instead", expectedResult, resultStr)
+        }
+    })
 }
