511b822fc83d15303aa342bd8004520735cdde7d
Artem Demo <artemdemo@gmail.com>
2019-11-22 18:13:11 +0200

    returned tests

--- tests.sh
+++ tests.sh
@@ -5,14 +5,14 @@ then
   go test ./binary_search ./quicksort ./tree ./graph -test.v -cover -coverprofile="coverage.out"
   go tool cover -html="coverage.out"
 else
-#  echo "binary_search/*"
-#  go test ./binary_search -test.v -cover
-#
-#  echo "quicksort/*"
-#  go test ./quicksort -test.v -cover
-#
-#  echo "tree/*"
-#  go test ./tree -test.v -cover
+  echo "binary_search/*"
+  go test ./binary_search -test.v -cover
+
+  echo "quicksort/*"
+  go test ./quicksort -test.v -cover
+
+  echo "tree/*"
+  go test ./tree -test.v -cover
 
   echo "graph/*"
   go test ./graph -test.v -cover
